dfb17d2a8b5ed6cc72edc3494758f95cf2b0350f
AraxnoAnarxo <57258354+AraxnoAnarxo@users.noreply.github.com>
2019-11-01 16:59:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print("Формула счастья")
 
-
-
 #определяет уровень самооценки по мотивам формулы У.Джеймса
 
 def formula_of_happiness(uspex, zhelaniye):
@@ -11,7 +9,6 @@ def formula_of_happiness(uspex, zhelaniye):
     else:
         happiness = uspex/zhelaniye
         return happiness
-
 
 print()
 print("Оцените уровень достигнутого от 1 до 10")
@@ -23,11 +20,10 @@ zhelaniye = int(zhelaniye)
 
 resultat = formula_of_happiness(uspex, zhelaniye)
 
-
-
 if (resultat < 1):
     print("У вас завышенный уровень притязаний. Вы можете доситчь счастья, увеличивая свой успех либо отказываясь от своих притязаний.")
 if (resultat > 1):
     print("У вас заниженный уровень притязаний. Вы можете доситчь счастья, увеличивая свои притязания.")
 if (resultat == 1):
     print("У вас адекватная самооценка")
+    
